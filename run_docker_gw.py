f6c533614db32a36feb6ffbb8ef5c8435ba66b57
Jonathan <jonl@softwareag.com>
2022-11-24 10:25:48 +0100

    Updated APIs

--- run_docker_gw.py
+++ run_docker_gw.py
@@ -12,6 +12,7 @@ except:
 # available import-options (apis) for apigateway
 poc_dict = {}
 
+
 # dict for PoC demos
 for file in os.listdir("./imports"):
 
@@ -29,8 +30,10 @@ elif len(sys.argv) == 2:
     if sys.argv[1] in options:
         key = sys.argv[1]
     else:
-        # will print all options if no valid option was given as additional call-parameter
-        # and asks for valid input
+        '''
+        will print all options if no valid option was given as additional call-parameter
+        and asks for valid input
+        '''
         key = 0
         for i in options:
             print(f"-\t{i}")
@@ -38,7 +41,10 @@ elif len(sys.argv) == 2:
         while not (key  in options or key == "full"):
             key = input("Insert one of the options above:\n> ")
 else:
-    # when no additional call-parameter is given, full demo will be selected for import
+    '''
+    when no additional call-parameter is given, 
+    full demo will be selected for import
+    '''
     key = "full"
 
 base = "http://localhost:5555/rest/apigateway"
@@ -47,11 +53,6 @@ loadbalancer = f"{base}/configurations/loadBalancer"
 healthy_gw = False
 max_iterations = 12
 sleep_s = 20
-# elasticsearch = 'docker run -d -e "discovery.type=single-node" -e "xpack.security.enabled=false" -u elasticsearch --network api-gateway-network --name elastic --hostname elastic -p 9200:9200 --net-alias elastic docker.elastic.co/elasticsearch/elasticsearch:8.2.3'
-# devportal = 'docker run -d -e SPRING_ELASTICSEARCH_REST_URIS="http://elastic:9200" --network api-gateway-network --name devportal --hostname devportal -p 80:8083 sagcr.azurecr.io/devportal:10.15'
-# statuscall_addr = "http://localhost:9200/_status"
-# healthy_es = False
-
 
 # checks if npm exists
 location = os.getenv('APPDATA')+'\\npm'
@@ -70,52 +71,20 @@ header_gw = {
 }
 
 
-# will check if wsl is needed and if so if .wslconfig exists
-# will create .wslconfig if it doesn_t exist
+'''
+checks if wsl is needed and if so if .wslconfig exists
+-> will create .wslconfig if it doesn't exist
+'''
 wsl_config_path = os.getenv('USERPROFILE')+"\.wslconfig"
 if (subprocess.call(f"wsl --status", shell=True) ==  0) and not(os.path.exists(wsl_config_path)):
     with open(wsl_config_path, "w") as f:
         wsl_settings = "[wsl2]\nmemory = 8GB\nkernelCommandLine = sysctl.vm.max_map_count=262144 && sysctl.fs.file-max=200000 && ulimit -n 65536"
         f.write(wsl_settings)
-        # main = "wsl -d docker-desktop"
-        # subprocess.call(f"{main} ulimit -n 65536",shell=True)
-        # subprocess.call(f"{main} sysctl -w fs.file-max=200000",shell=True)
-        # subprocess.call(f"{main} sysctl -w vm.max_map_count=262144",shell=True)
 
 x = input("Do you want to compose API Gateway? (by default 'n') [y/n]\n> ").lower()
 if x in {"y", "yes", "j"}:
     print("starting API GW via docker compose...")
     return_code = subprocess.call(f"{docker_run}", shell=True)
-
-    # start elasticsearch
-    # subprocess.call(f"{elasticsearch}", shell=True)
-
-    # # Will exit when {max_iterations} is reached 
-    # for iterations in range(max_iterations):
-    #     try:
-    #         # calls elasticsearch to check health
-    #         r = requests.get(url = statuscall_addr, headers = header_es)
-
-    #         if r.status_code == 200:
-    #             # will run devportal if http-call was successful
-    #             print("Elasticsearch is up and running! Initializing devportal...")
-    #             healthy_es = False
-    #             subprocess.call(f"{devportal}", shell=True)
-    #         else:
-    #             # waits {sleep_s} seconds till next http-call
-    #             print(f"Healthcheck failed! Retry in {sleep_s}s. [{iterations + 1}/{max_iterations}]")
-    #             iterations += 1
-    #             time.sleep(sleep_s)
-    #     except:
-    #         # waits {sleep_s} seconds till next http-call
-    #         print(f"Waiting for elasticsearch... [{iterations + 1}/{max_iterations}]")
-    #         iterations += 1
-    #         time.sleep(sleep_s)
-
-    # # will raise error when elasticsearch couldn't start in {max_iterations * sleep_s} seconds
-    # if(not(healthy_es)):
-    #     raise ConnectionError("Could not start elasticsearch and therefore was not able to install devportal!")
-
 
 # will exit when {max_iterations} is reached
 for iterations in range(max_iterations):
